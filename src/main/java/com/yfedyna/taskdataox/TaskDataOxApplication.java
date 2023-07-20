8e8399910e252ba8110b849c6b9c5c3ab2bd2934
yfedyna <fedynayaroslav@gmail.com>
2023-07-20 19:44:55 +0300

    init

---
+++ src/main/java/com/yfedyna/taskdataox/TaskDataOxApplication.java
@@ -0,0 +1,13 @@
+package com.yfedyna.taskdataox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskDataOxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskDataOxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yfedyna/taskdataox/TaskDataOxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yfedyna.taskdataox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskDataOxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
